3e454ab8e0b3159c57c8e8c67da90c5e6d5d212c
ppolanco01 <35773523+ppolanco01@users.noreply.github.com>
2018-02-26 09:48:24 -0600

    yahtzee

--- Yahtzee/yahtzee.js
+++ Yahtzee/yahtzee.js
@@ -20,23 +20,25 @@ function loadDice() {
       img.className = "";
     }
   });
+  document.getElementById('throwsRemain').innerHTML = yahtzee.throwsRemaniningInTurn;
+  document.getElementById('throwsRemainLabel').innerHTML = yahtzee.throwsRemaniningInTurn;
+  document.getElementById('roll').disabled = (yahtzee.throwsRemaniningInTurn <= 0);
 }
 
- function loadScorecard() {
-   // make TR and TD and insert into scoreRows
-   yahtzee.scoreCard.forEach(function(scoreCardRow) {
-     if (scoreCardRow.top) {
-       buildScoreCardRow(scoreCardRow.title, scoreCardRow.score);
-     }
-   });
-   buildScoreCardRow("Top Subtotal", " ");
-   buildScoreCardRow("Top Bonus", " ");
-   yahtzee .scoreCard.forEach(function(scoreCardRow) {
-     if (!scoreCardRow.top) {
-       buildScoreCardRow(scoreCardRow.title, scoreCardRow.score);
-     }
-   })
-   buildScoreCardRow("Total Score", " ");
+function loadScorecard() {
+  yahtzee.scoreCard.forEach(function(scoreCardRow) {
+    if (scoreCardRow.top) {
+      buildScoreCardRow(scoreCardRow.title, scoreCardRow.score);
+    }
+  });
+  buildScoreCardRow("Top Subtotal", " ");
+  buildScoreCardRow("Top Bonus", " ");
+  yahtzee .scoreCard.forEach(function(scoreCardRow) {
+    if (!scoreCardRow.top) {
+      buildScoreCardRow(scoreCardRow.title, scoreCardRow.score);
+    }
+  })
+  buildScoreCardRow("Total Score", " ");
 }
 
 function buildScoreCardRow(title, score) {
@@ -48,4 +50,28 @@ function buildScoreCardRow(title, score) {
   td2.innerHTML = score;
   tr.appendChild(td2);
   document.getElementById('scoreRows').appendChild(tr);
+}
+
+function rollDice() {
+  if (yahtzee.throwsRemaniningInTurn > 0) {
+    yahtzee.dice.forEach(function (die) {
+      if (!die.saved) {
+        die.sideUp = Math.floor(Math.random() * 6) + 1;
+
+      }
+    });
+    if (rerolled)
+    yahtzee.throwsRemaniningInTurn--;
+    loadDice();
+  }
+}
+
+function saveDie(dieIndex) {
+  if (yahtzee.throwsRemaniningInTurn != 3) {
+    yahtzee.dice.forEach(function (die) {
+      if (!die.saved) {
+        die.sideUp = Math.floor(Math.random() * 6) + 1;
+      }
+    }
+}
 }
